4d3aa82b6a065eae237a91bbc6eaf7652200f6e9
Zeeshan Hussain <shani_cool82@hotmail.com>
2020-01-14 17:15:12 +0500

    Add Patient Feature added.

--- script.js
+++ script.js
@@ -11,51 +11,170 @@
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Reference messages collection
-var messagesRef = firebase.database().ref('patients');
+// Get a reference to the database service
+var database = firebase.database();
+var userRef = database.ref("patients/")
 
-// Listen for form submit
-document.getElementById('patientForm').addEventListener('submit', submitForm);
+var users = [];
+//Get realtime data
+database.ref("patients/").on('value', function (snapshot) {
+    var tableRef = document.getElementById("patientTable");
+    tableRef.innerHTML = "";
 
-// Submit form
-function submitForm(e){
-  e.preventDefault();
+    // console.log(snapshot.val())
+    if (!snapshot.val())
+        return;
 
-  // Get values
-  var name = getInputVal('name');
-  var age = getInputVal('age');
-  var male = getInputVal('male');
-  var female = getInputVal('female');
-  var disease = getInputVal('disease');
-  var medication = getInputVal('medication');
-  var aptDate = getInputVal('aptDate');
-  var charges = getInputVal('charges');
-  var contact = getInputVal('contact');
+    patients = Object.values(snapshot.val());
+    var j = 0;
+    for (key in snapshot.val()) {
+        patients[j++].key = key
+    }
+    console.log(patients);
 
-  // Save message
-  saveMessage(name, age, male, female, disease, medication, aptDate, charges, contact);
+    for (var i = 0; i < patients.length; i++) {
+        tableRef.innerHTML += `
+            <table class="ptTable">
+            <tr>
+                <th width=120>Name: </th>
+                <td>${patients[i].name}</td>
+            </tr>
+            <tr>
+                <th>Age: </th>
+                <td>${patients[i].age}</td>
+            </tr>
+            <tr>
+                <th>Gender: </th>
+                <td>${patients[i].gender}</td>
+            </tr>
+            <tr>
+                <th>Disease: </th>
+                <td>${patients[i].disease}</td>
+            </tr>
+            <tr>
+                <th>Medication: </th>
+                <td>${patients[i].medication}</td>
+            </tr>
+            <tr>
+                <th>Appt. Date: </th>
+                <td>${patients[i].aptDate}</td>
+            </tr>
+            <tr>
+                <th>Fee: </th>
+                <td>${patients[i].charges}</td>
+            </tr>
+            <tr>
+                <th>Contact Details: </th>
+                <td>${patients[i].contact}</td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                <button onclick="getPatient('${patients[i].key}')" data-toggle="modal"
+                data-target="#addPatient" class="btn btn-sm btn-warning">Edit Record</button>  
+                <button onclick="deletePatient('${patients[i].key}')" class="btn btn-sm btn-danger">Delete Record</button>
+                </td>
+            </tr>
+            </table>
+            `
+    }
+});
 
-    // Clear form
-  document.getElementById('patientForm').reset();
+//Add Patient Function
+function addPatient() {
+    var patientRef = document.getElementById('patientForm');
+    var gender;
+    var ele = document.getElementsByName('gender'); 
+            for(i = 0; i < ele.length; i++) { 
+                if(ele[i].checked) 
+                gender = ele[i].value; 
+            } 
+
+    // Getting a unique key to push new record
+    var newPatientKey = firebase.database().ref().child('patients').push().key;
+
+    database.ref("patients/" + newPatientKey).set({
+        name: patientRef.name.value,
+        age: patientRef.age.value,
+        gender: gender,
+        disease: patientRef.disease.value,
+        medication: patientRef.medication.value,
+        aptDate: patientRef.aptDate.value,
+        charges: patientRef.charges.value,
+        contact: patientRef.contact.value
+    });
 }
 
-// Function to get get form values
-function getInputVal(id){
-  return document.getElementById(id).value;
-}
-
-// Save message to firebase
-function saveMessage(name, age, male, female, disease, medication, aptDate, charges, contact){
-  var newMessageRef = messagesRef.push();
-  newMessageRef.set({
-    name: name,
-    age:age,
-    male:male,
-    female:female,
-    disease:disease,
-    medication:medication,
-    aptDate:aptDate,
-    charges:charges,
-    contact:contact,
+//Get Patient Function
+function getPatient(patientKey) {
+  var name = document.getElementById("name");
+  var age = document.getElementById("age");
+  var gender;
+    var ele = document.getElementsByName('gender'); 
+            for(i = 0; i < ele.length; i++) { 
+                if(ele[i].checked) 
+                gender = ele[i].value; 
+            }
+  var disease = document.getElementById("disease");
+  var medication = document.getElementById("medication");
+  var aptDate = document.getElementById("aptDate");
+  var charges = document.getElementById("charges");
+  var contact = document.getElementById("contact");
+  
+  var sendPatientData = document.getElementById("sendPatient");
+  let editData = database.ref("patients/" + patientKey + "/").once("value", data => {
+    dbData = data.val();
+    name.value = dbData.name;
+    age.value = dbData.age;
+    gender.value = dbData.gender;
+    disease.value = dbData.disease;
+    medication.value = dbData.medication;
+    aptDate.value = dbData.aptDate;
+    charges.value = dbData.charges;
+    contact.value = dbData.contact;
+    
   });
+  let key = patientKey;
+  sendPatientData.onclick = function() {
+    updatePatient(patientKey, name, age, gender, disease, medication, aptDate, charges, contact);
+  };
+  sendPtaientData.innerHTML = "Update Record";
+}
+
+//Update Patient Function
+  function updatePatient(patientKey, name, age, gender, disease, medication, aptDate, charges, contact) {
+    var sendPatientData = document.getElementById("sendPatient");
+    var gender;
+    var ele = document.getElementsByName('gender'); 
+            for(i = 0; i < ele.length; i++) { 
+                if(ele[i].checked) 
+                gender = ele[i].value; 
+            }
+
+  database.ref("patients/" + patientKey).update({
+    name: name.value,
+    age: age.value,
+    gender: gender.value,
+    disease: disease.value,
+    medication: medication.value,
+    aptDate: aptDate.value,
+    charges: charges.value,
+    contact: contact.value
+  });
+  name.value = "";
+  age.value = "";
+  gender.value = "";
+  disease.value = "";
+  medication.value = "";
+  aptDate.value = "";
+  charges.value = "";
+  contact.value = "";
+  
+  sendPatientData.innerHTML = "Add Record";
+}
+
+//Delete Patient Function
+function deletePatient(patientKey) {
+    database.ref("patients/" + patientKey).remove()
 }
